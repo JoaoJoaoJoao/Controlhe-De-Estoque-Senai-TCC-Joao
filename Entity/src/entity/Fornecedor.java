011a70722e0afc065165c4d903993d1126335dba
joao_victor1 <joao_victor1@SNSOO1EP01VM145.saojose.sc.senai.br>
2014-09-04 20:33:31 -0300

    Primara Parte Classes

---
+++ Entity/src/entity/Fornecedor.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class Fornecedor {
+
+    private int idFornecedor;
+    private String nomeFornecedor;
+    private String telefoneFornecedor;
+    private int cnpj;
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public String getNomeFornecedor() {
+        return nomeFornecedor;
+    }
+
+    public void setNomeFornecedor(String nomeFornecedor) {
+        this.nomeFornecedor = nomeFornecedor;
+    }
+
+    public String getTelefoneFornecedor() {
+        return telefoneFornecedor;
+    }
+
+    public void setTelefoneFornecedor(String telefoneFornecedor) {
+        this.telefoneFornecedor = telefoneFornecedor;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
---
+++ Entity/src/entity/Produto.java
@@ -0,0 +1,44 @@
+package entity;
+
+public class Produto {
+
+    private int idProduto;
+    private String nomeProduto;
+    private double custoProduto;
+    private double valorProduto;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getCustoProduto() {
+        return custoProduto;
+    }
+
+    public void setCustoProduto(double custoProduto) {
+        this.custoProduto = custoProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(double valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+    
+    
+    
+}
