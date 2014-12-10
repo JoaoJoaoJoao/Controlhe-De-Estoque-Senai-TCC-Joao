fc192f8e5244fa3c3ea901194d9bc72933d70919
joao_victor1 <joao_victor1@SNSOO1EP01VM121.saojose.sc.senai.br>
2014-12-10 18:52:18 -0200

    asfs

--- Entity/src/dao/Script.sql
+++ Entity/src/dao/Script.sql
@@ -2,25 +2,20 @@ create database ProjetoTCC;
 use ProjetoTCC;
 
 
-CREATE TABLEIF NOT EXISTS ProjetoTCC.CategoriaProduto(
-  idCategoriaProduto INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE ProjetoTCC.CategoriaProduto(
+  idCategoriaGrupoProduto INT NOT NULL AUTO_INCREMENT,
   nomeGrupo VARCHAR(50) NOT NULL,
-  PRIMARY KEY (idCategoriaProduto));
+  PRIMARY KEY (idCategoriaGrupoProduto));
 
 CREATE TABLE  ProjetoTCC.Produto (
-  idProduto INT NOT NULL AUTO_INCREMENT,
+  idProduto INT NOT NULL AUTO_INCREMENT primary key,
   nomeProduto VARCHAR(100) NOT NULL,
   custoProduto DOUBLE NOT NULL,
   valorProduto DOUBLE NOT NULL,
   estoqueAtual INT NULL,
   estoqueMin INT NOT NULL,
-  unidade INT NULL,
-  CategoriaProduto_idCategoriaProduto INT NOT NULL,
-  PRIMARY KEY (idProduto),
-  INDEX fk_Produto_CategoriaProduto_idx(CategoriaProduto_idCategoriaProduto ASC),
-  CONSTRAINT fk_Produto_CategoriaProduto
-    FOREIGN KEY (CategoriaProduto_idCategoriaProduto)
-    REFERENCES ProjetoTCC.CategoriaProduto (idCategoriaProduto)
+  unidade INT not NULL,
+  categoria varchar (50) not null
    );
 
 
